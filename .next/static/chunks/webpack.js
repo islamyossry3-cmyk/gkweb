8b5d7b4ec2aa468be8a4507d84102860a96889a0
islamyossry3-cmyk <islamyossry3@gmail.com>
2025-12-22 11:06:29 +0200

    Updated app-build-manifest.json

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "538ea393738b15a7"; }
+/******/ 		__webpack_require__.h = function() { return "8d665ba368f9966e"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
