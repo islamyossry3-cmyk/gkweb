51c227e68dceb4ec920d9a9f92e3033b65a38daa
islamyossry3-cmyk <islamyossry3@gmail.com>
2025-12-23 22:30:31 +0200

    Updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,10 @@
 /** @type {import('next').NextConfig} */
-const basePath = process.env.NEXT_PUBLIC_BASE_PATH || '';
-
 const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
-  basePath,
-  assetPrefix: basePath,
   images: {
-    formats: ['image/avif', 'image/webp']
+    formats: ['image/avif', 'image/webp'],
+    unoptimized: false
   }
 };
 
